1d6d5a01ec50c39d939247a46bd268ef601917a4
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-03-17 09:09:43 +0300

    -

--- script/services.js
+++ script/services.js
@@ -1,10 +1,13 @@
 let serv = document.getElementById('services');
 
-for(i=0; i<3; i++){
+for(i=0; i<DATAservices.length; i++){
     serv.innerHTML += `
+<hr>
+<div class="service">
     <div class="title">${DATAservices[i].TITLE}</div>
     <div class="note">${DATAservices[i].NOTE}</div>
     <div class="price">${DATAservices[i].PRICE} ₽</div>
-    <div class="more">---</div>
+    <div class="more">Подробнее</div>
+</div>
 `
 }
