173967d03ef5110d00698d75f9c51cc262d6b00b
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-03-17 14:12:58 +0300

    -

--- script/services.js
+++ script/services.js
@@ -7,7 +7,7 @@ for(i=0; i<DATAservices.length; i++){
     <div class="title">${DATAservices[i].TITLE}</div>
     <div class="note">${DATAservices[i].NOTE}</div>
     <div class="price">${DATAservices[i].PRICE} ₽</div>
-    <div class="more">Подробнее</div>
+    <div class="more">→</div>
 </div>
 `
 }
