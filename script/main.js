a85857dec69e378421ad2de9fa5c7e1c69b3db2d
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-03-18 03:25:32 +0300

    -

--- script/main.js
+++ script/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    $('.slider').slick();
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        waitForAnimate: true,
+        centerMode: true,
+    });
 });
